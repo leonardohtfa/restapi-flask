ac8c440194f878a960c972571b0c4a3a74b39d17
leonardotfa <dev.leonardotfa@gmail.com>
2023-08-14 16:10:42 -0300

    feat: add healthcheck endpoint

--- application/app.py
+++ application/app.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 from flask_restful import Resource, reqparse
-from .model import UserModel
+from .model import UserModel, HealthCheckModel
 from mongoengine import NotUniqueError
 import re
 
@@ -32,6 +32,14 @@ _user_parser.add_argument('birth_date',
                           help="This field cannot be blank."
                           )
 
+class HealthCheck(Resource):
+    def get(self):
+        response = HealthCheckModel.objects(status="healthcheck")
+        if response:
+            return "Healthy", 200
+        else:
+            HealthCheckModel(status="healthcheck").save()
+            return "Healthy", 200
 
 class Users(Resource):
     def get(self):
